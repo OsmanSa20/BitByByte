bf490f04506752b7c2a37d950e8cffae4a798275
OsmanSa20 <osmansallamci@gmail.com>
2026-05-08 19:27:08 +0200

    email input

--- BadUX/src/views/LoginPage.vue
+++ BadUX/src/views/LoginPage.vue
@@ -4,11 +4,13 @@
 
 
     <div id='input-container'>
-            <p> Please Input E-mail </p>
-            <input class="e-input" type="text" placeholder="Enter E-mail" />
+            <div id="email-input">
+                <p> Please Input Emeil </p>
+                <input class="e-input" type="text" placeholder="Enter E-mail" />
+            </div>
             <div class="e-input-group">
                 <p> Please Input Passwort </p>
-                <input class="e-input" name='input' type="password" placeholder="Enter Passwort" />
+                <input class="e-input" name='input' type="password" placeholder="Enter Password" />
                 <span class="e-input-group-icon e-input-popup-date"></span>
             </div>
     </div>
@@ -25,8 +27,20 @@ export default defineComponent({
 </script>
 
 <style scoped>
-h1, .input-container {
-  font-family: Comic Sans;
+
+h1, #input-container {
+  font-family: Comic Sans MS;
 } 
+
+#email-input {
+  margin-left: 80px;
+}
+
+#email-input p {
+  font-size: 20px;
+  color: #333;
+}
+
+
 
 </style>
