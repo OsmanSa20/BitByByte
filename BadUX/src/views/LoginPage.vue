d3fd9768d796b684a11b3f756065594d7f200e4e
Osman <osman@eduh3009.hs-esslingen.de>
2026-05-08 19:16:40 +0200

    haltdeinmaulilir

--- BadUX/src/views/LoginPage.vue
+++ BadUX/src/views/LoginPage.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
     <h1>Login</h1>
+
+
+    <div id='input-container'>
+            <p> Please Input E-mail </p>
+            <input class="e-input" type="text" placeholder="Enter E-mail" />
+            <div class="e-input-group">
+                <p> Please Input Passwort </p>
+                <input class="e-input" name='input' type="password" placeholder="Enter Passwort" />
+                <span class="e-input-group-icon e-input-popup-date"></span>
+            </div>
+    </div>
+
   </div>
 </template>
 
@@ -13,5 +25,8 @@ export default defineComponent({
 </script>
 
 <style scoped>
+h1, .input-container {
+  font-family: Comic Sans;
+} 
 
 </style>
